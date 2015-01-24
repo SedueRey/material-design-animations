acc7c66c3c3c27265d9c0b73ea7b35ab270af1b7
Eduardo Rey <proyectos@seduerey.com>
2015-01-24 17:02:26 +0100

    some animations

--- material/js/core.js
+++ material/js/core.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 	$('.button_container .icon','#buttons').click( function(){
+		$('.button_container','#buttons').removeClass('active');
 		$(this).parents('.button_container').toggleClass('active');
 	} );
 });
